eebd9a82e41a8d93a8f9a1a9f438876a522099ef
gabde002 <gabde002@fiu.edu>
2021-08-19 19:08:26 -0400

    added js for todo list

--- manifest/todo.js
+++ manifest/todo.js
@@ -1,41 +1,22 @@
 
-    let toDoListArray = [];
-
-    function addTodo(text){
-        const todo = {
-            text,
-            checked: false,
-            id: Date.now(),
-        };
-        todoItems.push(todo);
-        console.log(todoItems);
-    }
-
-    const form = document.querySelector("#button");
-    form.addEventListener('submit', event =>{
-        event.preventDefault();
-        const input = form.querySelector("#form__input");
-
-        const text = input.value.trim();
-        if(text !== ''){
-            addTodo(text);
-            input.value='';
-            input.focus();
-        }
-
-    });
-
-   
+ $(document).ready(function(){
+     $("#add").on('click', function(){
+         var value =$("#todoList").val();
+         if(value!== ''){
+             var element = $("<li></li>").text(value);
+             var btn = $("<button class='rem'>X</button>");
+             $(elem).append(btn);
+             $('#todoList').append(elem);
+             $('#todoList').val("");
+             $
+         }
 
 
+     })
 
-
-
-
-    
-
-
-
+ })
+ 
+  
     
 
 
