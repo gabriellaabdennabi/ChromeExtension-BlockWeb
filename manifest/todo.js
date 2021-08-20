f48cdc554e6bcbb54ac8a25919765f983b81b0b1
gabde002 <gabde002@fiu.edu>
2021-08-20 19:34:44 -0400

    Added function for todo list to appear when todo button is pressed

--- manifest/todo.js
+++ manifest/todo.js
@@ -1,21 +1,30 @@
+$(function(){
 
+ 
  $(document).ready(function(){
      $("#submit_todo").on('click', function(){
          var value =$("#todo__input").val();
          if(value!== ''){
              var element = $("<li></li>").text(value);
-             var btn = $("<button class='rem'>X</button>");
-             $(element).append(btn);
              $('#todoList').append(element);
              $('#todoList').val(" ");
+             
              
          }
 
 
      })
 
- })
- 
+    
+     })
+
+
+     $("#todolist").on('click', function(){
+         $("#container__todo").fadeToggle();
+     })
+
+
+    })
   
     
 
