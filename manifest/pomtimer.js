453794d7350f4fa5fe0a67c546175a80b0091a30
gabde002 <gabde002@fiu.edu>
2021-08-21 19:15:43 -0400

    Added variables and timer object

--- manifest/pomtimer.js
+++ manifest/pomtimer.js
@@ -3,5 +3,27 @@ $(function(){
         $("#container__timer").fadeToggle();
     })
 
+    var pomodoro = {
+        started: false,
+        minutes: 0,
+        seconds: 0,
+        fillerHeight: 0,
+        fillerIncrement: 0,
+        interval: null,
+        minutesDom: null,
+        secondsDom: null,
+        fillerDom: null,
+        init: function(){
+            var self = this;
+            this.minutesDom= $("#minutes");
+            this.secondsDom=$("#seconds");
+            this.fillerDom=$("#filler");
+            this.interval = setInterval(function(){
+                self.intervalCallback.apply(self);
+            },1000);
+            
+        }
+    }
+
     
 })
